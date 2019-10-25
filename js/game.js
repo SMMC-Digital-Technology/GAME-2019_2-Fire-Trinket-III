89b0ad87114cb4462b6af8bc064a87e730e9720d
Pat a Cut of Ham <CUNNINPT@mackillop.act.edu>
2019-10-25 12:27:17 +1100

    Preloading
    This is putting many files into the preload function and changing the file names to have correct spelling.

--- js/game.js
+++ js/game.js
@@ -8,10 +8,11 @@ game.state.add('level', levelState);
 game.state.add('gameover', gameoverState);
 
 game.global = {
-   // set variables that travel between the states here
-   // eg score = 0;
-   // access using game.global
-   // eg game.gloabl.score
+  var turn = 0;
+  // set variables that travel between the states here
+  // eg score = 0;
+  // access using game.global
+  // eg game.gloabl.score
 };
 
 game.state.start('boot');
