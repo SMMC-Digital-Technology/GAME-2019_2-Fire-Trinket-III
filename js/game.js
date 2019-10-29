2a0d615199c6d6f1900601a5474d54205c84bcda
Pat a Cut of Ham <CUNNINPT@mackillop.act.edu>
2019-10-30 10:27:00 +1100

    Creating the levels
    This code creates the 6 different level states

--- js/game.js
+++ js/game.js
@@ -4,6 +4,12 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 //game.state.add('level', levelState);
+game.state.add('level1', level1State);
+game.state.add('level2', level2State);
+game.state.add('level3', level3State);
+game.state.add('level4', level4State);
+game.state.add('level5', level5State);
+game.state.add('level6', level6State);
 // remember to add in additional levels as they are made
 game.state.add('gameover', gameoverState);
 
