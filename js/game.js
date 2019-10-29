719ba10c190f859a5d4c156dcd8f4f2b983138e3
Pat a Cut of Ham <CUNNINPT@mackillop.act.edu>
2019-10-29 11:29:26 +1100

    Comments out some lines
    comments this line which is asking for a non existant state

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(1000, 800, Phaser.CANVAS, 'game-world');
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
-game.state.add('level', levelState);
+//game.state.add('level', levelState);
 // remember to add in additional levels as they are made
 game.state.add('gameover', gameoverState);
 
